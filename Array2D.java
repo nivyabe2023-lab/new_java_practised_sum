46ffd40be0597c41bd40daae0d56a72be25833f6
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-06-24 22:26:54 +0530

    Array2D_java

---
+++ Array2D.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Array2D {
+    public static void main(String[]args){
+        int rows=4;
+        int columns=2;
+        int arr[][] ={{1,3},{1,2},{2,3},{2,4}};
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+        }
+    }
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+        }
+    }
+            System.out.print(arr[0][1]);
+}
+    
+}
