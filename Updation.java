ebb2adf8f4b2524064cd75f696b6fe761e50e69a
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-06-26 22:13:21 +0530

    Updation.java

---
+++ Updation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Updation {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        for(int i=0;i<arr.length;i++){
+             arr[i] = sc.nextInt();
+        }
+        
+        int position = 3;
+        int value = 40;
+        arr[position] = value;
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
