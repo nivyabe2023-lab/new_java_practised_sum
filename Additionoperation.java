0499477b09c41045773f351579490e014f982541
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-06-26 22:12:58 +0530

    ddition_operation.class

---
+++ Additionoperation.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Additionoperation {
+    static  int Addition(){
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        return c;
+    }
+    public static void main(String[]args){
+        System.out.println(Addition());
+    }
+    
+}
